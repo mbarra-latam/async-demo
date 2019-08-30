d8cb30004f1235fad5a59221a6219d1d8e9a2961
Miguel Barra <mbarra.latam@gmail.com>
2019-08-29 20:42:53 -0400

    8- Consuming Promises

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 console.log('Before');
-getUser(1, user => {
-	console.log('User', user);
-	
-	// Get the repositories
-	getRepositories(user.gitHubUsername, repos => {
-		console.log('Repos', repos);
-	});
-});;
+
+getUser(1)
+	.then(user => getRepositories(user.gitHubUsername))
+	.then(repos => getCommits(repos[0]))
+	.then(commits => console.log('Commits', commits))
+	.catch(err => console.log('Error', err.message));
+
 console.log('After');
 
-function getUser(id, callback) {
-	setTimeout(() => {
-		console.log("Reading a user from a database...");
-		callback({ id: id, gitHubUsername: 'Mosh' });
-	}, 2000);
+function getUser(id) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log("Reading a user from a database...");
+			resolve({ id: id, gitHubUsername: 'Mosh' });
+		}, 2000);
+	});
 }
 
-function getRepositories(username, callback) {
-	setTimeout(() => {
-		console.log('Calling GitHub API...');
-		callback(['repo1', 'repo2', 'repo3']);
-	}, 2000);
+function getRepositories(username) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Calling GitHub API...');
+			resolve(['repo1', 'repo2', 'repo3']);
+		}, 2000);
+	});
+}
+
+function getCommits(repo) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Calling GitHub API...');
+			resolve(['commit']);
+		}, 2000);
+	});
 }
